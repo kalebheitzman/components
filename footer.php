a036c35baf89980b6efd5e5141cea53500f47651
Kaleb Heitzman <kalebheitzman@gmail.com>
2019-04-19 14:43:31 -0400

    added header and footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Components Theme Footer
+ */
+wp_footer(); ?>
+
+<footer id="footer">
+  
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Components Theme Index Page
+ */
+get_header(); ?>
+ 
+<h1><?php the_title(); ?></h1>
+
+<?php get_footer(); ?>
