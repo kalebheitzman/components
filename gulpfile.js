3b5b5202546f1bd6a914711778efac12716b38ce
Kaleb Heitzman <kalebheitzman@gmail.com>
2019-07-12 01:05:51 -0400

    added live reload

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sassGlob = require('gulp-sass-glob');
 var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
 
 sass.compiler = require('node-sass');
 
@@ -15,10 +16,12 @@ gulp.task('sass', function () {
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(livereload());
 });
 
 // watch sass
 gulp.task('default', function() {
+    livereload.listen();
     gulp.watch('./ui/**/*.scss', gulp.series('sass'));
 });
