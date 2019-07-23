018c2cc10f4404da8c710717a4a143fb0cec1072
Kaleb Heitzman <kalebheitzman@gmail.com>
2019-07-23 15:11:50 -0400

    updating structure of website

--- home.php
+++ home.php
@@ -6,6 +6,7 @@ get_header(); ?>
  
 <div class="site-main">
     <h1><?php the_title(); ?></h1>
+    <?php the_content() ?>
 </div>
 
 <?php get_footer(); ?>
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -5,7 +5,8 @@
  * 
  * Retreives a component based on the $component folder and $partial that
  * has been set. If no partial has been set, partial is automatically assumed
- * to be the same name as the component itself.
+ * to be the same name as the component itself. Reusable components can be found in 
+ * one of two folders, either components or layout.
  * 
  * @since 1.0.0
  */
@@ -14,20 +15,29 @@ function get_component( $component = null, $partial = null ) {
         return;
     }
     // define components path
-    $components = STYLESHEETPATH . "/ui/components";
+    $components = [
+        STYLESHEETPATH . "/ui/components",
+        STYLESHEETPATH . "/ui/layout",        
+    ];
 
-    // set partial to component
+    // set partial to component if not specified
     if ( is_null( $partial ) ) {
         $partial = $component;
     }
 
     // set component path
-    $component_path = $components . '/' . $component . '/' . $partial . '.php';
+    $component_path = $components[0] . '/' . $component . '/' . $partial . '.php';
+    $layout_path = $components[1] . '/' . $component . '/' . $partial . '.php';
 
     // load the component
     if ( file_exists( $component_path ) ) {
         include_once($component_path);
-    } 
+    }
+
+    // component doesnt exist, try layout
+    elseif ( file_exists( $layout_path ) ) {
+        include_once($layout_path);
+    }
 
     // fail silently
     else {
