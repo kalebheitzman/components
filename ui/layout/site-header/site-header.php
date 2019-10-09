9858b977cdbd0fcad24836455e5c67007754c0aa
Kaleb Heitzman <kalebheitzman@gmail.com>
2019-10-09 13:57:33 -0400

    cleaned up code

--- ui/layout/site-header/site-header.php
+++ ui/layout/site-header/site-header.php
@@ -1,3 +1,11 @@
-<header class="site-header">
-    <h1><?php echo get_bloginfo('name') ?></h1>
+<?php
+/**
+ * Site Header
+ *
+ * @package Components
+ * @since 1.0.0
+ */
+
+?><header class="site-header">
+	<h1><?php bloginfo( 'name' ); ?></h1>
 </header>
---
+++ ui/layout/site-navigation/site-navigation.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Site Navigation
+ *
+ * @package Components
+ * @since 1.0.0
+ */
+
+?><div class="site-navigation">
+	<nav>
+
+	</nav>
+</div>
