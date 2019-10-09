20a371523849bb1196f24264bf68d0d58a58553b
Kaleb Heitzman <kalebheitzman@gmail.com>
2019-10-09 13:23:54 -0400

    cleaned up code

--- footer.php
+++ footer.php
@@ -1,10 +1,14 @@
-<?php 
+<?php
 /**
  * Components Theme Footer
+ *
+ * @package Components
+ * @since 1.0.0
  */
+
 wp_footer(); ?>
 
-<?php get_component( 'site-footer' ) ?>
+<?php get_component( 'site-footer' ); ?>
 
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * components functions and definitions
+ * Components functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
@@ -25,6 +25,7 @@ if ( ! function_exists( 'components_setup' ) ) :
 		load_theme_textdomain( 'components', get_template_directory() . '/languages' );
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
+
 		/*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
@@ -32,6 +33,7 @@ if ( ! function_exists( 'components_setup' ) ) :
 		 * provide it for us.
 		 */
 		add_theme_support( 'title-tag' );
+
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
@@ -39,25 +41,37 @@ if ( ! function_exists( 'components_setup' ) ) :
 		 */
 		add_theme_support( 'post-thumbnails' );
 		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', '_s' ),
-		) );
+		register_nav_menus(
+			array(
+				'menu-1' => esc_html__( 'Primary', '_s' ),
+			)
+		);
+
 		/*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
 		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+		);
 		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'components_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'components_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => '',
+				)
+			)
+		);
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 		/**
@@ -65,12 +79,15 @@ if ( ! function_exists( 'components_setup' ) ) :
 		 *
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 250,
+				'width'       => 250,
+				'flex-width'  => true,
+				'flex-height' => true,
+			)
+		);
 	}
 endif;
 add_action( 'after_setup_theme', 'components_setup' );
@@ -79,15 +96,31 @@ add_action( 'after_setup_theme', 'components_setup' );
  * Enqueue scripts and styles.
  */
 function components_scripts() {
-	wp_enqueue_style( 'normalize-css', get_template_directory_uri() . '/css/normalize.css' );
-	wp_enqueue_style( 'components-style', get_template_directory_uri() . '/css/style.css' );
-	//wp_enqueue_script( 'components-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	//wp_enqueue_script( 'components-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-	// if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-	// 	wp_enqueue_script( 'comment-reply' );
-	// }
-	wp_enqueue_script( 'fontloader_js', get_template_directory_uri() . '/js/webfont.js', null, '', true );
-	wp_add_inline_script( 'fontloader_js', 'WebFont.load({google:{families:["Lato:100,300,400,700", "Glyph:300"]}});', 'after' );
+	wp_enqueue_style(
+		'normalize-css',
+		get_template_directory_uri() . '/css/normalize.css',
+		null,
+		'1.0.0'
+	);
+	wp_enqueue_style(
+		'components-style',
+		get_template_directory_uri() . '/css/style.css',
+		null,
+		'1.0.0'
+	);
+	wp_enqueue_script(
+		'fontloader_js',
+		get_template_directory_uri() . '/js/webfont.js',
+		null,
+		'1.6.28',
+		true
+	);
+
+	wp_add_inline_script(
+		'fontloader_js',
+		'WebFont.load({google:{families:["Lato:100,300,400,700", "Glyph:300"]}});',
+		'after'
+	);
 }
 add_action( 'wp_enqueue_scripts', 'components_scripts' );
 
