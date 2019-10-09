7296f83930599669eba518685cc1709024ff3cd5
Kaleb Heitzman <kalebheitzman@gmail.com>
2019-10-09 13:26:07 -0400

    cleaned up code

--- gulpfile.js
+++ gulpfile.js
@@ -10,18 +10,18 @@ sass.compiler = require('node-sass');
 
 // compile sass
 gulp.task('sass', function () {
-    return gulp
-        .src('./ui/style.scss')
-        .pipe(sassGlob())
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./css'))
-        .pipe(livereload());
+	return gulp
+		.src('./ui/style.scss')
+		.pipe(sassGlob())
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./css'))
+		.pipe(livereload());
 });
 
 // watch sass
 gulp.task('default', function() {
-    livereload.listen();
-    gulp.watch('./ui/**/*.scss', gulp.series('sass'));
+	livereload.listen();
+	gulp.watch('./ui/**/*.scss', gulp.series('sass'));
 });
