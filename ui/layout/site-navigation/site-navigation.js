f420d2a5b74fd1454fd2b89c9c8f896f5677ca19
Kaleb Heitzman <kalebheitzman@gmail.com>
2019-10-10 07:44:01 -0400

    added resonsive menu

--- ui/layout/site-navigation/site-navigation.js
+++ ui/layout/site-navigation/site-navigation.js
@@ -1,7 +1,28 @@
 ;(function($) {
   $(function() {
-    // DOM Ready
-		// Insert all scripts here
-		alert('works');
+
+		$('nav ul li > a:not(:only-child)').click(function(e) {
+      $(this)
+        .siblings('.sub-menu')
+        .toggle()
+      $('.sub-menu')
+        .not($(this).siblings())
+        .hide()
+			e.stopPropagation()
+			e.preventDefault()
+		})
+
+    $('html').click(function() {
+      $('.sub-menu').hide()
+		})
+
+		$('.nav-toggle').on('click', function() {
+			this.classList.toggle('active')
+		})
+
+		$('.nav-toggle').click(function() {
+			$('nav ul').toggle()
+		})
+
   })
 })(jQuery)
