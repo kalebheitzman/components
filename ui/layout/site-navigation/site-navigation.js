8e6431af92b38146a0fae9e954f7f4d2eadc7a71
Kaleb Heitzman <kalebheitzman@gmail.com>
2019-10-10 15:38:06 -0400

    adding js magic to the navmenu

--- ui/layout/site-navigation/site-navigation.js
+++ ui/layout/site-navigation/site-navigation.js
@@ -1,19 +1,37 @@
 ;(function($) {
   $(function() {
 
-		$('nav ul li > a:not(:only-child)').click(function(e) {
-      $(this)
-        .siblings('.sub-menu')
-        .toggle()
-      $('.sub-menu')
-        .not($(this).siblings())
-        .hide()
+		// detect menu with submenu click
+		$('.has-sub-menu.level-0').click(function(e) {
+			// add an attribute specifiying menu is open
+			let open = $(this).attr('data-submenu-open')
+			let state = open === 'false' ? 'true' : 'false'
+			$(this).attr('data-submenu-open', state)
+
+			// hide all other sub menus
+			$('.has-sub-menu.level-0').not($(this)).attr('data-submenu-open', 'false')
+
+			// // if submenu is open, stop click propogation
+			console.log($(this).attr('data-submenu-open'))
+			if ($(this).attr('data-submenu-open') === 'true') {
+				e.preventDefault()
+			}
 			e.stopPropagation()
-			e.preventDefault()
-		})
+		});
+
+		// $('nav ul li > a:not(:only-child)').click(function(e) {
+    //   $(this)
+    //     .siblings('.sub-menu')
+    //     .toggle()
+    //   $('.sub-menu')
+    //     .not($(this).siblings())
+    //     .hide()
+		// 	e.stopPropagation()
+		// 	e.preventDefault()
+		// })
 
     $('html').click(function() {
-      $('.sub-menu').hide()
+			$('[data-submenu-open="true"]').attr('data-submenu-open', 'false')
 		})
 
 		$('.nav-toggle').on('click', function() {
