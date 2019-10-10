b99532fbbc4b65d7b51c76a207f491a8619c67a5
Kaleb Heitzman <kalebheitzman@gmail.com>
2019-10-10 17:49:08 -0400

    added responsive colors and styles

--- ui/layout/site-navigation/site-navigation.js
+++ ui/layout/site-navigation/site-navigation.js
@@ -1,46 +1,49 @@
 ;(function($) {
   $(function() {
 
-		// detect menu with submenu click
-		$('.has-sub-menu.level-0').click(function(e) {
+		/**
+		 * Detect anchor click with submenu children
+		 */
+		$('.has-sub-menu').click(function(e) {
 			// add an attribute specifiying menu is open
 			let open = $(this).attr('data-submenu-open')
 			let state = open === 'false' ? 'true' : 'false'
 			$(this).attr('data-submenu-open', state)
 
-			// hide all other sub menus
-			$('.has-sub-menu.level-0').not($(this)).attr('data-submenu-open', 'false')
+			// hide all other sub menus at this data level
+			let dataLevel = $(this).attr('data-level')
+			$('.has-sub-menu.level-' + dataLevel)
+				.not($(this))
+				.attr('data-submenu-open', 'false')
 
 			// // if submenu is open, stop click propogation
-			console.log($(this).attr('data-submenu-open'))
 			if ($(this).attr('data-submenu-open') === 'true') {
 				e.preventDefault()
 			}
 			e.stopPropagation()
 		});
 
-		// $('nav ul li > a:not(:only-child)').click(function(e) {
-    //   $(this)
-    //     .siblings('.sub-menu')
-    //     .toggle()
-    //   $('.sub-menu')
-    //     .not($(this).siblings())
-    //     .hide()
-		// 	e.stopPropagation()
-		// 	e.preventDefault()
-		// })
-
+		/**
+		 * Detect clicks away from the menu to close submenus
+		 */
     $('html').click(function() {
 			$('[data-submenu-open="true"]').attr('data-submenu-open', 'false')
 		})
 
+		/**
+		 * Toggle the hamburger menu item
+		 */
 		$('.nav-toggle').on('click', function() {
 			this.classList.toggle('active')
+			$('.primary-menu').toggleClass('active')
 		})
 
-		$('.nav-toggle').click(function() {
-			$('nav ul').toggle()
-		})
+		// /**
+		//  *
+		//  */
+		// $('.nav-toggle').click(function() {
+		// 	$('nav ul').toggle()
+		// })
 
   })
 })(jQuery)
